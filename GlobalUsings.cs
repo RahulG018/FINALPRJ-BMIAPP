89e4bd4e91c7562035ed8ff9a3783ec0160eb4b5
Anilkumarputta <114547466+Anilkumarputta@users.noreply.github.com>
2022-12-12 10:57:34 +0530

    Update GlobalUsings.cs

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -3,3 +3,5 @@ global using SkiaSharp;
 global using SkiaSharp.Views.Maui.Controls.Hosting;
 global using CommunityToolkit.Mvvm.Input;
 global using CommunityToolkit.Mvvm.ComponentModel;
+global using MauiBMICalculator.Views;
+global using MauiBMICalculator.ViewModels;
